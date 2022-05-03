bad97fb01d7110b63dc13acef13b50fea381adbe
arunskum <arunskum@amazon.nl>
2022-05-03 16:07:47 +0200

    create aft-sandbox control tower account

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    AccountEmail              = "arunskum+tfsanbdox@amazon.com"
+    AccountName               = "aft-sandbox"
+    ManagedOrganizationalUnit = "SharedServices"
+    SSOUserEmail              = "arunskum+tfsanbdox@amazon.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
